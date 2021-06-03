532819efadd5c960ed8974cfd7f033bb405fe598
zhz364 <zhz364@ucsd.edu>
2021-06-02 19:15:54 -0700

    Update user auth validations for username

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -7,14 +7,32 @@ router.route('/').get((req,res) =>{
         .catch(err => res.status(400).json('Error:' + err));
 });
 
-router.route('/add').post((req,res) => {
-    const {username, password }= req.body;
-    const newUser = new User({username});
-    console.log(username)
-    console.log(password)
-    newUser.save()
-        .then(( )=> res.json("Successfuly Created a new User"))
-        .catch(err => res.status(400).json('Error:' + err));
+router.route('/add').post(async(req,res) => {
+    try{
+        const {username, password }= req.body;
+        const newUser = new User({username});
+
+        if(!username || !password){
+            return res.status(400).json("Error: "+ "Please enter all required fields")
+        }
+
+        if(password.length < 6){
+            return res.status(400).json("Error: "+ "Please enter a password of at least 6 characters")
+        }
+
+        const existingUser = await User.findOne({username:username})
+        if (existingUser){
+            return res.status(400).json("Error: "+ "An account with this User name already exists")
+        }
+
+        newUser.save()
+            .then(( )=> res.json("Successfuly Created a new User"))
+    }catch(err){
+        res.status(400).json('Error: ' + err);
+    }
+    
+    
+     
 })
 
 module.exports = router;
