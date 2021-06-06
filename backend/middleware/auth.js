a8dec3daa38852c3e2282bf29e70842fd83c474c
zhz364 <zhz364@ucsd.edu>
2021-06-06 08:19:48 -0700

    Finish auth middleware

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,8 +1,15 @@
+const jwt = require("jsonwebtoken");
+
 function auth(req,res,next){
     try{
         const token = req.cookies.token;
-        console.log(token)
-        if(!token) return res.status(401).jason({errorMessage:"Unauthorized token"})
+        if(!token) return res.status(401).jason({errorMessage:"Unauthorized token"});
+        
+        const verified = jwt.verify(token,process.env.JWT_SECRET);
+        
+        req.user = verified.user;
+
+        next();
     }catch(err){
         res.status(401).json({errorMessage: "Unauthorized"});
     }
