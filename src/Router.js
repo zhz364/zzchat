8ed284eba6f822e789b4c09f0af0864ba5ba3e69
zhz364 <zhz364@ucsd.edu>
2021-06-07 16:07:48 -0700

    Update Router.js

---
+++ src/Router.js
@@ -0,0 +1,21 @@
+import React from "react";
+import {BrowserRouter, Switch, Route} from "react-router-dom";
+import Register from "../src/components/auth/Register"
+
+function Router(){
+    return <BrowserRouter>
+        <Switch>
+            <Route path="/">
+                <div>Home</div>
+            </Route>
+            <Route path="/register">
+                <Register />
+            </Route>
+            <Route path="/login">
+                <div>login</div>
+            </Route>
+        </Switch>
+    </BrowserRouter>
+};
+
+export default Router;
