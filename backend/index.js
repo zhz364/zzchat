33be4fb2156d59a6c015ad5a2e6235c05ecd99c5
zhz364 <zhz364@ucsd.edu>
2021-06-10 21:24:57 -0700

    Fix mongodb connection issue

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ app.use(cors({
 }));
 
 const uri = process.env.ATLAS_URI;
-mongoose.connect(uri,{useNewUrlParser: true, useCreateIndex: true});
+mongoose.connect(uri,{useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true});
 const connection = mongoose.connection;
 connection.once('open',()=>{
     console.log("MongoDB successfully connected");
