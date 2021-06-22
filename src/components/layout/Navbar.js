d4b55b2bf6c01b07f54bd6b2956f548a276297b7
zhz364 <zhz364@ucsd.edu>
2021-06-21 23:10:12 -0700

    Put some css for navbar

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -2,22 +2,20 @@ import React, { useContext } from "react";
 import {Link} from "react-router-dom";
 import AuthContext from "../../context/AuthContext";
 import Logout from "../auth/Logout"
+import '../layout/navbar.css';
 function Navbar(){
 
     const { loggedIn } = useContext(AuthContext);
     console.log(loggedIn)
-    return <div>
-        <Link to="/">Home</Link>
+    return <div className="navbar-div">
+        <div className="navbar-logo"><Link to="/"><img src="../../../transparentLogo.png" width="15%" ></img></Link></div>
         {loggedIn === false && (
-                <>
-                    <Link to="/register">Register</Link>
-                    <Link to="/login">Login</Link>
-                </>
+                <div className="right-navbar">
+                    <div className="navbar-register"><Link to="/register"><button className="navbar-button">Register</button></Link></div>
+                    <div className="navbar-login"><Link to="/login"><button className="navbar-button">Login</button></Link></div>
+                </div>
         )}
         {loggedIn === true && <Logout/>}
-
-        
-
     </div>
 
 };
