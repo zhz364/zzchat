d37c93597ae4bf793b3d59cef765de82b8713b58
zhz364 <zhz364@ucsd.edu>
2021-06-22 22:11:22 -0700

    Add css for login page

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,7 +1,9 @@
 import React, {useContext, useState} from 'react';
 import axios from "axios";
+import {Link} from "react-router-dom";
 import AuthContext from '../../context/AuthContext';
 import { useHistory } from 'react-router-dom';
+import "../auth/login.css"
 
 function Login(){
     const [username, setUsername] = useState("");
@@ -16,7 +18,6 @@ function Login(){
                 username,
                 password
             }
-
             await axios.post("http://localhost:5000/users/login",loginData);
             await getLoggedIn();
             history.push("/")
@@ -25,13 +26,20 @@ function Login(){
         }
     }
 
-    return <div>
-        <h1>Login to your account</h1>
-        <form onSubmit={login}>
-            <input type="username" placeholder="Username" onChange={(e) => setUsername(e.target.value)} value={username}></input>
-            <input type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} value={password}></input>
-            <button type="submit">Login</button>
-        </form>
+    return <div className="login-div">
+        <h1 className="title">Welcome back!</h1>
+        <div className="login-form"><form onSubmit={login}>
+            <div className="option">EMAIL</div>
+            <div className="login-input-div"><input className="login-input" type="username" placeholder="Username" onChange={(e) => setUsername(e.target.value)} value={username}></input></div>
+            <div className="option">PASSWORD</div>
+            <div className="login-input-div"><input className="login-input" type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} value={password}></input></div>
+            <button className="login-btn" type="submit">Login</button>
+        </form></div>
+        <div className="other-option-div">
+            <div className="options-context">Need an account? <Link className="register-link" to="/register"><span className="login-register">Register</span></Link></div>
+            <div className="options-context">Use the Demo Account</div>
+        </div>
+        
     </div>;
 };
 
